46749bfc9063b4fdab95bc792c13dd4a206cf41f
Bartosz <bborek.agh@gmail.com>
2019-05-06 15:18:11 +0200

    Utorzenie plików (brak kodu)

---
+++ program/kalkulator/include/cal_BB.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Bartosz on 06.05.2019.
+//
+
+#ifndef PROGRAM_CAL_BB_HPP
+#define PROGRAM_CAL_BB_HPP
+#include <cmath>
+#include <iostream>
+
+#endif //PROGRAM_CAL_BB_HPP
---
+++ program/kalkulator/src/cal_BB.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Bartosz on 06.05.2019.
+//
+
