8d7c15027475fdeb0cda7a6fe056afcaa7bef10d
Bartosz <bborek.agh@gmail.com>
2019-05-06 18:06:30 +0200

    Dodanie funkcjii (prototyp)

--- program/kalkulator/include/cal_BB.hpp
+++ program/kalkulator/include/cal_BB.hpp
@@ -4,7 +4,25 @@
 
 #ifndef PROGRAM_CAL_BB_HPP
 #define PROGRAM_CAL_BB_HPP
-#include <cmath>
+#include <math.h>
 #include <iostream>
+
+template<typename T>
+T pow(T a , T b )       // Potęgowanie , pamiętać, że każda liczba podniesiona do potęgi 0 daje wynik 1
+{
+    return std::pow ;
+}
+
+
+template<typename T>
+
+T pierw( T x , int y ) ;      // Pierwiastkowanie
+
+
+
+float log(float l)                // Liczenie logarytmu naturalnego , należy pamiętać by x>0
+{
+    return std::log ;
+}
 
 #endif //PROGRAM_CAL_BB_HPP
--- program/kalkulator/src/cal_BB.cpp
+++ program/kalkulator/src/cal_BB.cpp
@@ -2,3 +2,14 @@
 // Created by Bartosz on 06.05.2019.
 //
 
+#include "calc_BB.hpp"
+
+
+template<typename T>
+
+T pierw( T x , int y ){
+
+    T z = 1/y ;
+
+    return pow(T x , T z) ;
+}
