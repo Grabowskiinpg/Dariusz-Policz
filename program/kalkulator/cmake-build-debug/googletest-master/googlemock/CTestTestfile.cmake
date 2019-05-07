d2f510cf10325a2ac2076aa2b04f28a04023539e
grendagit <49614758+grendagit@users.noreply.github.com>
2019-05-07 09:26:14 +0200

    Delete CTestTestfile.cmake

--- program/kalkulator/cmake-build-debug/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Bartosz/Desktop/Projekt_Kalkulator/program/googletest-master/googlemock
-# Build directory: C:/Users/Bartosz/Desktop/Projekt_Kalkulator/program/kalkulator/cmake-build-debug/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
