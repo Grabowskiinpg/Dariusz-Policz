47da4469ff7ca460d2fb71929929c03381b7940e
Grabowskiinpg <49510241+Grabowskiinpg@users.noreply.github.com>
2019-05-12 11:03:04 +0200

    Obiekty
    Rozpocząłęm pracę na Wielomianem, nie skończyłem jeszcze skonstruować dla niego kontruktora.

--- program/kalkulator/include/calc_MGr.hpp
+++ program/kalkulator/include/calc_MGr.hpp
@@ -4,19 +4,29 @@
 
 #ifndef KALKULATOR_CALC_MGR_HPP
 #define KALKULATOR_CALC_MGR_HPP
+# include "calc_MG.hpp"
 
+template<typename T>
 class Monomial{ //jednomian
 private:
-    template<typename T> numeric_factor_;
+    T numeric_factor_;
     int index_;
 public:
-template<typename T>
     Monomial(T numeric_factor, int index):numeric_factor_(numeric_factor),index_(index){}
-    template<typename T>
     Monomial(T numeric_factor):Monomial(numeric_factor,1){}
     Monomial(int index): Monomial(1,index){}
     Monomial():Monomial(1,1){}
+
+    T get_num() const { return numeric_factor_;}
+    T get_index() const { return index_;}
 };
 
+template<typename T>
+class Polynomial{
+private:
+    std::vector<Monomial<T>> polynomial_;
+public:
+    Polynomial(const std::vector<Monomial<T>>& vector);
+};
 
 #endif //KALKULATOR_CALC_MGR_HPP
--- program/kalkulator/src/calc_MGr.cpp
+++ program/kalkulator/src/calc_MGr.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "calc_MGr.hpp"
+
+template<typename T>
+Polynomial<T>::Polynomial(const std::vector<Monomial<T>>& vector){
+    int max_ind=0;
+    for(Monomial<T> monomial : vector) {
+        if(monomial.get_index()>max_ind){
+            max_ind=monomial.get_index();
+        }
+    }
+    for(int i=0;i<=max_ind;i++){
+        polynomial_[i]=Monomial(0,i);
+    }
+    for(Monomial<T> monomial : vector) {
+        polynomial_[monomial.get_index()].
+    }
+}
