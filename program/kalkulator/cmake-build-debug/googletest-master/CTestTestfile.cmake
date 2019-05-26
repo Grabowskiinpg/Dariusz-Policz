c40da39640c6b6ea8ac6b22500fc7fb6bc9c8f69
Zyrafau <rfk712@gmail.com>
2019-05-27 00:22:41 +0200

    testy integralOfPolynomial

--- program/kalkulator/cmake-build-debug/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zyrafau/calc_inpg/program/googletest-master
-# Build directory: /home/zyrafau/calc_inpg/program/kalkulator/cmake-build-debug/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
